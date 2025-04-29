513713e567ff5321932bcf5d7c16d94971caba8f
NiLuJe <ninuje@gmail.com>
2025-04-29 03:46:42 +0200

    Stricter Page: matching

--- data/extract_data.py
+++ data/extract_data.py
@@ -232,7 +232,7 @@ def parse_page(data: dict) -> dict:
 	# Try to restore categories on Page: pages...
 	if title.startswith("Page:"):
 		for book_title, cats in BOOK_CATEGORIES.items():
-			if book_title in title:
+			if title[5:].startswith(book_title):
 				logger.opt(colors=True).info(f"Restored categories from <cyan>{book_title}</cyan>")
 				categories |= cats
 				break
@@ -282,7 +282,7 @@ def main() -> None:
 
 		categories = page["categories"]
 		for book_title, cats in BOOK_CATEGORIES.items():
-			if book_title in title:
+			if title[5:].startswith(book_title):
 				logger.opt(colors=True).info(
 					f"Restored categories to <green>{title}</green> from <cyan>{book_title}</cyan>"
 				)
