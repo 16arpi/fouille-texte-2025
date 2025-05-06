16arpi/fouille-texte-2025

commit 00158cf113d6c19b034e1474e99cd5349608d75e

Fix sed invocation on macOS

Makefile
@@ -62,7 +62,7 @@ create_environment:
 	$(PYTHON_INTERPRETER) -m venv --system-site-packages --prompt $(VENV_NAME) .venv
 	@echo ">>> New virtualenv created. Activate with:"
 	@echo "source .venv/bin/activate"
-	@sed -e 's/($(VENV_NAME)) /$(VENV_NAME)/g' -i .venv/bin/activate
+	@sed -e 's/($(VENV_NAME)) /$(VENV_NAME)/g' -i '' .venv/bin/activate
 
 
 

commit a734b23b2a27cd7534216608e40a472e2c245c15

Unbreak sed invocation on Linux
I thought that form was also supported, alas, nope, macOS is the worst.

Regression since 00158cf113d6c19b034e1474e99cd5349608d75e

Makefile
@@ -62,13 +62,18 @@ sync_data_up:
 		s3://tal-m1-fouille/data
 
 
+UNAME_S := $(shell uname -s)
 ## Set up Python interpreter environment
 .PHONY: create_environment
 create_environment:
 	$(PYTHON_INTERPRETER) -m venv --system-site-packages --prompt $(VENV_NAME) .venv
 	@echo ">>> New virtualenv created. Activate with:"
 	@echo "source .venv/bin/activate"
+ifeq ($(UNAME_S),Darwin)
 	@sed -e 's/($(VENV_NAME)) /$(VENV_NAME)/g' -i '' .venv/bin/activate
+else
+	@sed -e 's/($(VENV_NAME)) /$(VENV_NAME)/g' -i .venv/bin/activate
+endif
 
 
 