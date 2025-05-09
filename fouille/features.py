b66747b817df66fe8ab3deefa6a000a223490b47
NiLuJe <ninuje@gmail.com>
2025-05-09 20:52:58 +0200

    Final linitng pass

--- fouille/features.py
+++ fouille/features.py
@@ -50,9 +50,15 @@ def vectorize(input_parquet: Path) -> None:
 	X_train, X_test, y_train, y_test = train_test_split(X, cats, test_size=0.2, random_state=0)
 
 	print("to parquet")
-	pd.DataFrame(X_train.toarray(), columns=columns).to_parquet(f"{folder}/X_train.parquet", index=False, compression="zstd")
-	pd.DataFrame(X_test.toarray(), columns=columns).to_parquet(f"{folder}/X_test.parquet", index=False, compression="zstd")
-	pd.DataFrame(y_train, columns=["semicentury"]).to_parquet(f"{folder}/y_train.parquet", index=False, compression="zstd")
+	pd.DataFrame(X_train.toarray(), columns=columns).to_parquet(
+		f"{folder}/X_train.parquet", index=False, compression="zstd"
+	)
+	pd.DataFrame(X_test.toarray(), columns=columns).to_parquet(
+		f"{folder}/X_test.parquet", index=False, compression="zstd"
+	)
+	pd.DataFrame(y_train, columns=["semicentury"]).to_parquet(
+		f"{folder}/y_train.parquet", index=False, compression="zstd"
+	)
 	pd.DataFrame(y_test, columns=["semicentury"]).to_parquet(f"{folder}/y_test.parquet", index=False, compression="zstd")
 
 
